e6329ecef8559b1c356228d0e1440a1d6008267c
sue <dldustn2040@gmail.com>
2022-12-30 23:39:02 +0900

    useParams

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 
 export default function ProductDetail() {
 
-    const location = useLocation();
-    const state = location.state;
-    const [product, setProduct] = useState({});
+    const {id} = useParams();
 
-    useEffect(() => {
-        setProduct(state);
-    }, [state]);
+    // const location = useLocation();
+    // const state = location.state;
+    // const [product, setProduct] = useState({});
+
+    // useEffect(() => {
+    //     setProduct(state);
+    // }, [state]);
 
     return (
         <div>
-            <img src={product.url} />
-            <div>{product.name}</div>
+            {/* <img src={product.url} /> */}
+            <div>{id}</div>
         </div>
     )
 }
